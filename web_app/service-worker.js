46a3f4a1931f536845360510aaafb6e9d453009a
francerferreira <francer.ferreira@users.noreply.github.com>
2026-04-21 01:28:35 -0400

    Limpa tela de login do web mobile

--- web_app/service-worker.js
+++ web_app/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "cf-checklist-frota-pwa-20260421-02";
+const CACHE_NAME = "cf-checklist-frota-pwa-20260421-03";
 const APP_SHELL = [
     "./",
     "./index.html",
     "./manifest.json",
-    "./static/css/styles.css?v=20260421-01",
+    "./static/css/styles.css?v=20260421-03",
     "./static/js/app-20260419-25.js?v=20260421-01",
     "./static/icons/icon-192.png",
     "./static/icons/icon-512.png",
